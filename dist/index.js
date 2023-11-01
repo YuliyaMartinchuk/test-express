d42334e1e63dfc309efd8d977b7e8500dc4dd39e
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-11-01 21:56:09 +0300

    added about

--- dist/index.js
+++ dist/index.js
@@ -9,6 +9,9 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('Hello,  this is test');
 });
+app.get('/about', (req, res) => {
+    res.send('This is my about route..... ');
+});
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,15 @@ app.get('/', (req:Request, res:Response) => {
     res.send('Hello,  this is test')
 })
 
+app.get('/about', (req, res) => {
+    res.send('This is my about route..... ')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
 
 // Export the Express API
 module.exports = app
